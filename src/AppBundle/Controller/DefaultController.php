d4385c9c1adb504436a77a7910ebd83def1654d3
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2016-12-11 14:17:03 +0100

    added assetic, added jquery to appbundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('AppBundle:Default:Landing.html.twig');
     }
 }
