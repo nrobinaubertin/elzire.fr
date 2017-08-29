551f36e639e4724524ada3b81406383e1d008eb6
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2017-08-29 09:46:31 +0200

    Add php7-fileinfo extension to docker image

--- docker/start.sh
+++ docker/start.sh
@@ -21,7 +21,7 @@ pathToProject=$(pwd)
 cd docker || exit
 
 sudo docker build \
-    --build-arg PHP_EXT="php7-common php7-imagick php7-mbstring php7-gd php7-intl php7-iconv php7-json php7-dom php7-ctype php7-xml php7-posix php7-session php7-tokenizer" \
+    --build-arg PHP_EXT="php7-common php7-imagick php7-mbstring php7-gd php7-intl php7-iconv php7-json php7-dom php7-ctype php7-xml php7-posix php7-session php7-tokenizer php7-fileinfo" \
     -t elzire .
 
 status=$?
