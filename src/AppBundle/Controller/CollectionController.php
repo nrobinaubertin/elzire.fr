8740fcbbb3befef70840ad2b8f48ca9d38252d7b
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2017-09-02 16:43:23 +0200

    Implement collection page

--- src/AppBundle/Controller/CollectionController.php
+++ src/AppBundle/Controller/CollectionController.php
@@ -22,31 +22,100 @@ class CollectionController extends Controller
             }
         }
 
+        $infos = $this->getFiles($collectionDir.$dir);
+
+        $main_image = "/image/mariages/" . $dir . "/" . $infos["main_image"];
+        foreach($infos["elements"] as $k=>$v) {
+            $infos["elements"][$k]["miniature"] = "/miniature/mariages/" . $dir . "/" . $v["miniature"];
+        }
+        return $this->renderHTML($dir, $main_image, $infos["elements"]);
+    }
+
+    public function getFiles($dir)
+    {
         // now we need to find all the elements of the collection
         $elements = [];
-        foreach(scandir($collectionDir.$dir) as $elementDir) {
+        $main_image = "";
+        foreach(scandir($dir) as $elementDir) {
             if($elementDir == "." || $elementDir == ".." || is_file($elementDir)) {
                 continue;
             }
-            $url = strtolower(preg_replace("/^\d+[_-]+/","", $elementDir));
+            // the "presentation" dir is here only for the main_image
+            if(preg_match("/^\d+[_-]presentation/",$elementDir)) {
+                foreach(scandir($dir."/".$elementDir) as $file) {
+                    if(
+                        is_file($dir."/".$elementDir."/".$file)
+                        && preg_match("/image/", mime_content_type($dir."/".$elementDir."/".$file))
+                    ) {
+                        $main_image = $elementDir."/".$file;
+                        break;
+                    }
+                }
+                continue;
+            }
             $element = array(
-                "name" => preg_replace("/[_-]/", " ", $url),
-                "url" => $url
+                "name" => $this->getName($elementDir),
+                "url" => $this->getNiceUrl($elementDir)
             );
-            foreach(scandir($collectionDir.$dir."/".$elementDir) as $file) {
+            // let's find the miniature for this element
+            foreach(scandir($dir."/".$elementDir) as $file) {
                 if(
-                    is_file($collectionDir.$dir."/".$elementDir."/".$file)
-                    && preg_match("/image/", mime_content_type($collectionDir.$dir."/".$elementDir."/".$file))
+                    is_file($dir."/".$elementDir."/".$file)
+                    && preg_match("/image/", mime_content_type($dir."/".$elementDir."/".$file))
                     && preg_match("/^V/", $file)
                 ) {
-                    $element["miniature"] = $file;
+                    $element["miniature"] = $elementDir."/".$file;
                     break;
                 }
             }
             $elements[] = $element;
         }
 
-        return New Response(var_export($elements));
-            
+        $infos = array(
+            "main_image" => $main_image,
+            "elements" => $elements
+        );
+        return $infos;
+    }
+
+    private function getNiceUrl($url) {
+        return strtolower(preg_replace("/^\d+[_-]+/","", $url));
+    }
+
+    private function getName($str) {
+        $name = preg_replace("/^\d+[_-](.+)/","$1",$str);
+        $name = preg_replace("/[_-]+/"," ",$name);
+        return $name;
+    }
+
+    private function renderHTML($dir, $main_image, $elements) {
+        $title = $this->getName($dir);
+        $breadcrumbs = array(
+            ["/", "Accueil"],
+            ["/mariages", "Mariages"],
+            ["/mariages/".$dir, $title] 
+        );
+        
+        $others = [];
+        $collectionDir = $this->get('kernel')->getRootDir() . '/../data/mariages/';
+        foreach(scandir($collectionDir) as $e) {
+            if($e == "." || $e == ".." || !is_dir($collectionDir.$e)) {
+                continue;
+            }
+            $others[] = array(
+                "url" => "/mariages/".$this->getNiceUrl($e),
+                "name" => $this->getName($e)
+            );
+        }
+
+        return $this->render('@App/collection.html.twig',array(
+            "main_image" => $main_image,
+            "elements" => $elements,
+            "breadcrumbs" => $breadcrumbs,
+            "categorie" => "Mariages - ".$title,
+            "others" => $others,
+            "title" => "",
+            "subtitle" => "",
+        ));
     }
 }
