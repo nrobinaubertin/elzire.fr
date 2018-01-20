57214351508373a1fbc975d861fff12ae8c4c433
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-01-20 08:43:05 +0000

    Start to prepare container for tls certificates

--- docker/server/run.sh
+++ docker/server/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-chown -R "${UID}:${GID}" /php /nginx /etc/s6.d /var/log/nginx /var/tmp/nginx
+#crontab -r
+#echo "@monthly certbot certonly -n --agree-tos --webroot -w /var/lib/letsencrypt --force-renewal --email $MAIL -d $DOMAIN --cert-name elzire --cert-path /certs/cert.pem --key-path /certs/privkey.pem --fullchain-path /certs/fullchain.pem --chain-path /certs/cert.pem" | crontab -
+
+# mkdir -p /letsencrypt/.well-known/acme-challenge
+# echo "plop" > /letsencrypt/.well-known/acme-challenge/plop
+
+chown -R "${UID}:${GID}" /php /nginx /etc/s6.d /var/log/nginx /var/tmp/nginx /letsencrypt
 exec su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
