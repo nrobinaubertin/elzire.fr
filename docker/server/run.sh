4b9d702446050215a9b1e24556b6c499d9a4fb1e
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-07-10 09:34:44 +0200

    Simplify server container

--- docker/server/run.sh
+++ docker/server/run.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-#crontab -r
-#echo "@monthly certbot certonly -n --agree-tos --webroot -w /var/lib/letsencrypt --force-renewal --email $MAIL -d $DOMAIN --cert-name elzire --cert-path /certs/cert.pem --key-path /certs/privkey.pem --fullchain-path /certs/fullchain.pem --chain-path /certs/cert.pem" | crontab -
-
-# mkdir -p /letsencrypt/.well-known/acme-challenge
-# echo "plop" > /letsencrypt/.well-known/acme-challenge/plop
-
-chown -R "${UID}:${GID}" /php /nginx /etc/s6.d /var/log/nginx /var/tmp/nginx /certs
+chown -R "${UID}:${GID}" /etc/s6.d /var/log/nginx /var/log/php7 /var/tmp/nginx /certs /etc/php7 /etc/nginx /run/nginx
 exec su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
