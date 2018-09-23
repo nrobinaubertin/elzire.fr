35de6ee7933e86249353e1c8ff31af9e5ddc7277
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-23 15:03:01 +0200

    Rework server container

--- docker/server/run.sh
+++ docker/server/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-chown -R "${UID}:${GID}" /etc/s6.d /var/log/nginx /var/log/php7 /var/tmp/nginx /certs /etc/php7 /etc/nginx /run/nginx
+chown -R "${UID}:${GID}" /etc/s6.d /var/log /var/tmp/nginx /etc/php7 /etc/nginx /run/nginx
 exec su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
