a2db6d492e3a4f0b317e331fdd06430a0897440d
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-23 19:32:05 +0200

    fixes

--- docker/autonomous/run.sh
+++ docker/autonomous/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-php bin/console cache:clear --env=prod --no-debug --no-warmup
+rm -rf /elzire/var/*
+php bin/console cache:clear --env=prod --no-debug
+mkdir -p /elzire/var/cache/thumbs
 chown -R "${UID}:${GID}" /etc/s6.d /var/log /var/tmp/nginx /etc/php7 /etc/nginx /run/nginx /elzire
+su-exec "${UID}:${GID}" php bin/console elzire:gen-thumbs &
 exec su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
